0362fe52987db8539fc011be9bbd26d6a376a4a0
lenovo <lenovo@lenovo-PC>
2019-02-27 21:44:36 +0530

    Maven project1

---
+++ Maven_Project/src/mvn/DemoA.java
@@ -0,0 +1,19 @@
+package mvn;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class DemoA {
+	static
+	{
+	    System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+
+	}
+@Test
+public void testA() {
+	WebDriver driver=new ChromeDriver();
+    driver.close();
+}
+
+}
